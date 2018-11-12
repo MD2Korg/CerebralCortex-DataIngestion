12ecbb1eaec35cc208a10d814ab50c0b664b47b2
Timothy W. Hnat <twhnat@memphis.edu>
2018-11-12 14:46:51 -0600

    Initial docker file for the data ingestion pipeline
    CC-635
    
    Co-Authored-By: ali <nasirali1@users.noreply.github.com>

--- run.sh
+++ run.sh
@@ -8,20 +8,20 @@
 export PYSPARK_PYTHON=/usr/bin/python3
 
 # export CerebralCortex path if CerebralCortex is not installed
-export PYTHONPATH="${PYTHONPATH}:/home/ali/IdeaProjects/CerebralCortex-2.0/"
+# export PYTHONPATH="${PYTHONPATH}:/home/ali/IdeaProjects/CerebralCortex-2.0/"
 
 # Update path to libhdfs.so if it's different than /usr/local/hadoop/lib/native/libhdfs.so
 # uncooment it if using HDFS as NoSQl storage
 #export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/hadoop/lib/native/libhdfs.so
 
 #Spark path, uncomment if spark home is not exported else where.
-export SPARK_HOME=/home/ali/spark/spark-2.2.1-bin-hadoop2.7/
+# export SPARK_HOME=/home/ali/spark/spark-2.2.1-bin-hadoop2.7/
 
 #PySpark args (do not change unless you know what you are doing)
 export PYSPARK_SUBMIT_ARGS="--packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.2.0 pyspark-shell"
 
 #set spark home, uncomment if spark home is not exported else where.
-export PATH=$SPARK_HOME/bin:$PATH
+# export PATH=$SPARK_HOME/bin:$PATH
 
 
 #########################################################################################
@@ -32,7 +32,7 @@ export PATH=$SPARK_HOME/bin:$PATH
 PARTICIPANTS=""
 
 # directory path where all the CC configurations are stored
-CONFIG_DIRECTORY="/home/ali/IdeaProjects/CerebralCortex-2.0/conf/"
+CONFIG_DIRECTORY="/cc_conf/"
 
 # spark master. This will work on local machine only. In case of cloud, provide spark master node URL:port.
 SPARK_MASTER="local[*]"
