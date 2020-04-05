68c236d0ac8102bbc3c92b54a84a1e753329b04f
nasirali1 <nasir.ali08@gmail.com>
2020-04-05 01:19:51 -0500

    changed date format

--- run.sh
+++ run.sh
@@ -36,7 +36,7 @@ export LD_LIBRARY_PATH="/usr/local/hadoop/lib/native/libhdfs.so"
 
 # directory path where all the CC configurations are stored
 CONFIG_DIRECTORY="/cerebralcortex/code/config/cc3_moods_conf/"
-DAY_TO_PROCESS=`date '+%m%d%Y'` # MMDDYYYY format
+DAY_TO_PROCESS=`date '+%Y%m%d'` # MMDDYYYY format
 HOUR_TO_PROCESS=`date -d '1 hours ago' '+%H'`
 BATCH_SIZE=200
 STUDY_NAME="mcontain"
